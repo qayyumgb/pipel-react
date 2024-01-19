834632d496aaed9fb2d66042489c990eb938ab58
abdul qayyum <qayyumgb96@gmail.com>
2024-01-19 18:08:15 +0500

    Revert "few changes for carousel item"
    This reverts commit 858381d829a9a50170a05f832181844517c9d368.

--- src/pages/carousel/carousel-list/carousel-list.tsx
+++ src/pages/carousel/carousel-list/carousel-list.tsx
@@ -1,102 +1,152 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./carousel-list.module.scss";
-import { HeroCard } from "../../../interfaces/heroCard";
-import { useUpdateInnerActiveMutation } from "../../../redux/slices/home";
+import {HeroCard} from "../../../interfaces/heroCard";
+import {useUpdateInnerActiveMutation} from "../../../redux/slices/home";
 import emptyState from "../../../assets/images/emptyState.svg";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import { Grid } from "@mui/material";
-import { DeleteItemModal } from "../../../modals/delete-item-modal/delete-item-modal";
-import { DraggableItem } from "../../../shared/dragabble-item/dragabble-item";
+import {DragDropContext, Droppable} from "react-beautiful-dnd";
+import {Grid} from "@mui/material";
+import {DeleteItemModal} from "../../../modals/delete-item-modal/delete-item-modal";
+import {DraggableItem} from "../../../shared/dragabble-item/dragabble-item";
 
 interface CarousalAddedProps {
-  carousalData: HeroCard[];
-  onEditData: (updatedData: HeroCard) => void;
-  onDeleteItem: (itemId: string) => void;
-  onUpdateIconClick: (item: any) => void;
-  onPreviewCarousel: (itemId: string) => void;
+    carousalData: HeroCard[];
+    onEditData: (updatedData: HeroCard) => void;
+    onDeleteItem: (itemId: string) => void;
+    onUpdateIconClick: (item: any) => void;
+    onPreviewCarousel: (itemId: string) => void;
 }
 
+const DUMMY_CAROUSEL_DATA: HeroCard[] = Array.from({length: 10}, (_, index) => ({
+    id: `item-${index + 1}`,
+    title: `Title ${index + 1}`,
+    description: `Description for item ${index + 1}.`,
+    order: index + 1,
+    active: index % 2 === 0,
+    image: `image${index + 1}.jpg`,
+    action: `Action ${index + 1}`,
+}));
 const CarouselList: React.FC<CarousalAddedProps> = ({
-  carousalData,
-  onEditData,
-  onDeleteItem,
-  onUpdateIconClick,
-  onPreviewCarousel,
-}) => {
-  const [items, setItems] = useState<HeroCard[]>(carousalData);
-  const [editedItemId, setEditedItemId] = useState<string | null>(null);
+                                                         carousalData,
+                                                         onEditData,
+                                                         onDeleteItem,
+                                                         onUpdateIconClick,
+                                                         onPreviewCarousel,
+                                                     }) => {
+    const [items, setItems] = useState<HeroCard[]>(carousalData);
+    const [editedItemId, setEditedItemId] = useState<string | null>(null);
 
-  const [updateInnerActive] = useUpdateInnerActiveMutation();
+    const [updateInnerActive] = useUpdateInnerActiveMutation();
 
-  const handleDragEnd = (result: any) => {
-    if (!result.destination) {
-      return;
-    }
-    const updatedItems = Array.from(items);
-    const [reorderedItem] = updatedItems.splice(result.source.index, 1);
-    updatedItems.splice(result.destination.index, 0, reorderedItem);
-    const updatedFormData = updatedItems.map((item, index) => ({
-      ...item,
-      order: index + 1,
-    }));
-    setItems(updatedFormData);
-  };
+    const handleDragEnd = (result: any) => {
+        if (!result.destination) {
+            return;
+        }
 
-  useEffect(() => {
-    setItems([...carousalData].sort((a, b) => a.order - b.order));
-  }, [carousalData]);
+        const updatedItems = Array.from(items);
+        const [reorderedItem] = updatedItems.splice(result.source.index, 1);
+        updatedItems.splice(result.destination.index, 0, reorderedItem);
+        const updatedFormData = updatedItems.map((item, index) => ({
+            ...item,
+            order: index + 1,
+        }));
+        setItems(updatedFormData);
+    };
 
-  const handleSubmit = (item: HeroCard[]) => {
-    console.log(`Save data for item with ID: ${item}`);
-  };
+    useEffect(() => {
+        setItems([...carousalData].sort((a, b) => a.order - b.order));
 
-  return (
-    <>
-      <Grid item xs={12} className={styles.tableContainer}>
-        {carousalData.length > 0 ? (
-          <>
-            <form onSubmit={() => handleSubmit(items)}>
-              <DragDropContext onDragEnd={handleDragEnd}>
-                <Droppable droppableId="droppable">
-                  {(provided, snapshot) => (
-                    <div className={styles.Table} ref={provided.innerRef}>
-                      <div className={styles.thead}>
-                        <div className={styles.tr}>
-                          <span className={`${styles.th} w-5`}>סדר</span>
-                          <span className={`${styles.th} w-20`}>כותרת</span>
-                          <span className={`${styles.th} w-10`}>תמונה</span>
-                          <span className={`${styles.th} w-25`}>כתובת אתר</span>
-                          <span className={`${styles.th} w-25`}></span>
-                        </div>
-                      </div>
+    }, [ carousalData]);
 
-                      {carousalData.map((item: HeroCard, index: number) => (
-                        <DraggableItem<HeroCard> // Specify the type here
-                          key={item.id}
-                          item={item}
-                          index={index}
-                          onUpdateIconClick={onUpdateIconClick}
-                          onPreviewCarousel={onPreviewCarousel}
-                          // handleDeleteButtonClick={handleDeleteButtonClick}
-                        />
-                      ))}
-                      {provided.placeholder}
+
+
+    const handleCheckboxChange = async (itemId: string, checked: boolean) => {
+
+        setItems((prevItems) =>
+            prevItems.map((item) =>
+                item.id === itemId ? {...item, active: checked} : item
+            )
+        );
+
+        const payload = {
+            id: itemId,
+            active: checked,
+        };
+
+        try {
+            await updateInnerActive({payload});
+            console.log("API hit on Active Checkbox");
+        } catch (error) {
+            console.log("Error while updating checkbox state", error);
+        }
+    };
+
+    const handleEditData = (itemId: string, editedData: HeroCard) => {
+        const updatedItems = items.map((item) =>
+            item.id === itemId ? {...item, ...editedData} : item
+        );
+        setItems(updatedItems);
+        onEditData(editedData);
+    };
+
+    const handleEditButtonClick = (itemId: string) => {
+        setEditedItemId(itemId);
+    };
+
+
+    const handleSubmit = (item: HeroCard[]) => {
+        console.log(`Save data for item with ID: ${item}`);
+    };
+
+
+
+    return (
+        <>
+            <Grid item xs={12} className={styles.tableContainer}>
+                {DUMMY_CAROUSEL_DATA.length > 0 ? (
+                    <>
+                        <form onSubmit={() => handleSubmit(items)}>
+                            <DragDropContext onDragEnd={handleDragEnd}>
+                                <Droppable droppableId="droppable">
+                                    {(provided, snapshot) => (
+                                        <div className={styles.Table} ref={provided.innerRef}>
+                                            <div className={styles.thead}>
+                                                <div className={styles.tr}>
+                                                    <span className={`${styles.th} w-5`}>סדר</span>
+                                                    <span className={`${styles.th} w-20`}>כותרת</span>
+                                                    <span className={`${styles.th} w-10`}>תמונה</span>
+                                                    <span className={`${styles.th} w-25`}>כתובת אתר</span>
+                                                    <span className={`${styles.th} w-25`}></span>
+                                                </div>
+                                            </div>
+
+                                            {DUMMY_CAROUSEL_DATA.map((item: HeroCard, index: number) => (
+                                                <DraggableItem<HeroCard> // Specify the type here
+                                                    key={item.id}
+                                                    item={item}
+                                                    index={index}
+                                                    onUpdateIconClick={onUpdateIconClick}
+                                                    onPreviewCarousel={onPreviewCarousel}
+                                                    // handleDeleteButtonClick={handleDeleteButtonClick}
+                                                />
+                                            ))}
+                                            {provided.placeholder}
+                                        </div>
+                                    )}
+                                </Droppable>
+                            </DragDropContext>
+                        </form>
+                        <Grid container justifyContent="flex"></Grid>
+                    </>
+                ) : (
+                    <div className={styles.no_data_state}>
+                        <img src={emptyState} width={200} alt="Empty"/>
+                        <h3>No Items Added</h3>
                     </div>
-                  )}
-                </Droppable>
-              </DragDropContext>
-            </form>
-            <Grid container justifyContent="flex"></Grid>
-          </>
-        ) : (
-          <div className={styles.no_data_state}>
-            <img src={emptyState} width={200} alt="Empty" />
-            <h3>No Items Added</h3>
-          </div>
-        )}
-      </Grid>
-    </>
-  );
+                )}
+            </Grid>
+
+        </>
+    );
 };
 
 export default CarouselList;
--- src/pages/carousel/carousel.tsx
+++ src/pages/carousel/carousel.tsx
@@ -1,144 +1,180 @@
-import {
-  Container,
-  Grid,
-  ThemeProvider,
-  Typography,
-  createTheme,
-} from "@mui/material";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-import { ReactElement, useState } from "react";
-import flag1 from "../../assets/images/isreal.png";
-import flag2 from "../../assets/images/usFlag.png";
-import { HeroCard } from "../../interfaces";
-import { AddItemModal } from "../../modals/add-item-modal/add-item-modal";
+import React, {ReactElement, useState} from 'react';
+import {Button, Container, createTheme, Grid, ThemeProvider, Typography,} from '@mui/material';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import AddIcon from '@mui/icons-material/Add';
+import {HeroCard} from '../../interfaces';
+import {useUploadSomeDataMutation} from '../../redux/slices/home';
+import flag1 from '../../assets/images/isreal.png';
+import flag2 from '../../assets/images/usFlag.png';
+import styles from './carousel.module.scss';
+import {AddItemModal} from "../../modals/add-item-modal/add-item-modal";
+import CarouselList from './carousel-list/carousel-list';
+import ModalWrapper from "../../common/modal";
 import PreviewCarouselItem from "../home/components/preview-carousel-item/preview-carousel-item";
-import CarouselList from "./carousel-list/carousel-list";
-import styles from "./carousel.module.scss";
-import { Header } from "./components";
-import { DUMMY_CAROUSEL_DATA } from "./data";
-import { useCarousel } from "./hooks";
+
 
 export function Carousel(): ReactElement {
-  const [isCarouselFormOpen, setIsCarouselFormOpen] = useState(false);
-  const [isPreviewModal, setIsPreviewModal] = useState(false);
-  const [carousalData, setCarousalData] =
-    useState<HeroCard[]>(DUMMY_CAROUSEL_DATA);
+    const [isCarouselFormOpen, setIsCarouselFormOpen] = useState(false);
+    const [carousalData, setCarousalData] = useState<HeroCard[]>([]);
 
-  const [previewCarouselItem, setPreviewCarouselItem] = useState<string | null>(
-    null,
-  );
-  const [isRandomOrderActive, setRandomOrderActive] = useState<boolean>(false);
-  const {
-    checkBoxHandler,
-    handleEditData,
-    handleOpenCarouselDeleteModal,
-    handleAddData,
-  } = useCarousel({
-    setRandomOrderActive: setRandomOrderActive,
-    setCarousalData: setCarousalData,
-    isRandomOrderActive,
-  });
+    const [previewCarouselItem, setPreviewCarouselItem] = useState<
+        string | null
+    >(null);
+    const [isRandomOrderActive, setRandomOrderActive] = useState(false)
 
-  const onPreviewIconClick = (itemId: string) => {
-    setPreviewCarouselItem(itemId);
-  };
-  const [langauage, setLangauage] = useState("HE");
 
-  const handleLanguage = (event: any) => {
-    setLangauage(event);
-    console.log(langauage);
-  };
-  const customTheme = createTheme({
-    components: {
-      MuiTabs: {
-        styleOverrides: {
-          indicator: {
-            display: "none",
-          },
+    const [uploadSomeData] = useUploadSomeDataMutation();
+
+    const handleEditData = (updatedData: HeroCard) => {
+        setCarousalData((prevData) =>
+            prevData.map((item) => (item.id === updatedData.id ? updatedData : item))
+        );
+
+    };
+
+    const handleAddData = (newData: HeroCard) => {
+        setCarousalData((prevData) => [...prevData, newData]);
+    };
+
+    const checkBoxHandler = async () => {
+        const payload = {
+            // create a payload for submitting form
+        };
+
+        try {
+            await uploadSomeData({payload: payload});
+            console.log('API Hit for Random Order');
+
+            // show some toast or snackbar message for success
+        } catch (error) {
+            console.error('Error while adding form data', error);
+        } finally {
+            // in case if loader used, stop it here
+        }
+
+        setRandomOrderActive((prev) => !prev)
+    };
+
+
+    const handleOpenCarouselDeleteModal = (itemId: string) => {
+        // setIsDeleteModalOpen(true);
+    };
+
+
+    const onPreviewIconClick = (itemId: string) => {
+        setPreviewCarouselItem(itemId);
+    };
+    const [langauage, setLangauage] = useState('HE');
+
+    const handleLanguage = (event: any) => {
+        setLangauage(event);
+        console.log(langauage);
+    };
+    const customTheme = createTheme({
+        components: {
+            MuiTabs: {
+                styleOverrides: {
+                    indicator: {
+                        display: 'none',
+                    },
+                },
+            },
         },
-      },
-    },
-  });
-  const onUpdateIconClick = (item: any) => {
-    // setInitialFormObject(item);
-    setIsCarouselFormOpen(true);
-  };
-  const onAddButtonClick = () => {
-    // setInitialFormObject(INITIAL_FORM_OBJECT);
-    setIsCarouselFormOpen(true);
-  };
+    });
+    const onUpdateIconClick = (item: any) => {
+        // setInitialFormObject(item);
+        setIsCarouselFormOpen(true);
+    };
+    const onAddButtonClick = () => {
+        // setInitialFormObject(INITIAL_FORM_OBJECT);
+        setIsCarouselFormOpen(true);
+    };
 
-  const isLanguageSelected = (language: string) => langauage === language;
-  return (
-    <>
-      <Container>
-        <Grid item xs={12} className={styles.listWrapper}>
-          <Header
-            checkBoxHandler={checkBoxHandler}
-            onAddButtonClick={onAddButtonClick}
-            isRandomOrderActive={isRandomOrderActive}
-          />
+    const isLanguageSelected = (language: string) => langauage === language;
+    return (
+        <>
+            <Container>
+                <Grid item xs={12} className={styles.listWrapper}>
+                    <Grid
+                        container
+                        display={'flex'}
+                        alignItems={'center'}
+                        justifyContent={'space-between'}
+                    >
+                        <Grid item xs={6}>
+                            <h4 className={styles.heading}>ניהול קרוסלה</h4>
+                        </Grid>
+                        <Grid item xs={6} display={'flex'} justifyContent={'end'} gap={1}>
+                            <Button
+                                onClick={checkBoxHandler}
+                                className={`secondary-btn btn-round ${isRandomOrderActive ? 'randomBtn' : ''}`}
+                                variant="outlined"
+                                startIcon={<ShuffleIcon style={{marginLeft: 10, marginRight: 0}}/>}
+                                color="success"
+                            >
+                                סדר אקראי
+                            </Button>
+                            <Button
+                                variant="contained"
+                                startIcon={
+                                    <AddIcon style={{marginLeft: 10, marginRight: 0}}/>
+                                }
+                                className="primary-btn btn-round"
+                                color="success"
+                                onClick={onAddButtonClick}
+                            >
+                                הוסף פריט
+                            </Button>
+                        </Grid>
+                    </Grid>
 
-          <ThemeProvider theme={customTheme}>
-            <Tabs value={0} className={styles.tabWrapper}>
-              <Tab
-                className={`${styles.tabCustom} ${
-                  isLanguageSelected("HE") ? styles.activeTab : ""
-                }`}
-                onClick={() => handleLanguage("HE")}
-                label={
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <Typography variant="body1">עִברִית</Typography>
-                    <img src={flag1} alt="flag" className={styles.tabIcon} />
-                  </div>
-                }
-              />
+                    <ThemeProvider theme={customTheme}>
+                        <Tabs value={0} className={styles.tabWrapper}>
+                            <Tab
+                                className={`${styles.tabCustom} ${isLanguageSelected('HE') ? styles.activeTab : ''}`}
+                                onClick={() => handleLanguage('HE')}
+                                label={
+                                    <div style={{display: 'flex', alignItems: 'center'}}>
+                                        <Typography variant="body1">עִברִית</Typography>
+                                        <img src={flag1} alt="flag" className={styles.tabIcon}/>
+                                    </div>
+                                }
+                            />
 
-              <Tab
-                className={`${styles.tabCustom} ${
-                  isLanguageSelected("EN") ? styles.activeTab : ""
-                }`}
-                onClick={() => handleLanguage("EN")}
-                label={
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <Typography variant="body1">אנגלית</Typography>
-                    <img src={flag2} alt="flag" className={styles.tabIcon} />
-                  </div>
-                }
-              />
-            </Tabs>
-          </ThemeProvider>
-          <Grid>
-            <CarouselList
-              carousalData={carousalData}
-              onEditData={handleEditData}
-              onDeleteItem={handleOpenCarouselDeleteModal}
-              onUpdateIconClick={onUpdateIconClick}
-              onPreviewCarousel={onPreviewIconClick}
-            />
+                            <Tab
+                                className={`${styles.tabCustom} ${isLanguageSelected('EN') ? styles.activeTab : ''}`}
+                                onClick={() => handleLanguage('EN')}
+                                label={
+                                    <div style={{display: 'flex', alignItems: 'center'}}>
+                                        <Typography variant="body1">אנגלית</Typography>
+                                        <img src={flag2} alt="flag" className={styles.tabIcon}/>
+                                    </div>
+                                }
+                            />
+                        </Tabs>
+                    </ThemeProvider>
+                    <Grid>
+                        <CarouselList
+                            carousalData={carousalData}
+                            onEditData={handleEditData}
+                            onDeleteItem={handleOpenCarouselDeleteModal}
+                            onUpdateIconClick={onUpdateIconClick}
+                            onPreviewCarousel={onPreviewIconClick}
+                        />
+                        <ModalWrapper maxWidth='641px' open={Boolean(previewCarouselItem)}
+                                      onClose={() => setPreviewCarouselItem(null)}>
+                            <PreviewCarouselItem carousalData={carousalData}
+                                                 onClose={() => setPreviewCarouselItem(null)}
+                                                 selectedItemId={previewCarouselItem}/>
 
-            {isPreviewModal && (
-              <PreviewCarouselItem
-                carousalData={carousalData}
-                onClose={() => {
-                  setIsPreviewModal(false);
-                  setPreviewCarouselItem(null);
-                }}
-                selectedItemId={previewCarouselItem}
-                isShow={isPreviewModal}
-              />
-            )}
-          </Grid>
-        </Grid>
-      </Container>
-      {isCarouselFormOpen && (
-        <AddItemModal
-          isOpen={isCarouselFormOpen}
-          closeModal={() => setIsCarouselFormOpen(false)}
-          onSubmit={handleAddData}
-        />
-      )}
-    </>
-  );
-}
+                        </ModalWrapper>
+                    </Grid>
+                </Grid>
+            </Container>
+            <AddItemModal isOpen={isCarouselFormOpen} closeModal={() => setIsCarouselFormOpen(false)}/>
+        </>
+    );
+};
+
