62703364d95e9d0df0175ec4003a0d222debd5c9
abdul qayyum <qayyumgb96@gmail.com>
2024-01-29 11:14:01 +0500

    preview, add/edit/delete and language setting for carousel list

--- src/pages/carousel/data/index.ts
+++ src/pages/carousel/data/index.ts
@@ -3,12 +3,24 @@ import { HeroCard } from "../../../interfaces";
 export const DUMMY_CAROUSEL_DATA: HeroCard[] = Array.from(
   { length: 3 },
   (_, index) => ({
-    id: `item-${index + 1}`,
+    id: `English-${index + 1}`,
     title: `Title ${index + 1}`,
-    description: `Description for item ${index + 1}.`,
+    description: `Description for English ${index + 1}.`,
     order: index + 1,
     active: index % 2 === 0,
     image: `image${index + 1}.jpg`,
-    action: `Action ${index + 1}`,
-  }),
+    action: `http://www.english.com/${index + 1}`,
+  })
+);
+export const DUMMY_CAROUSEL_DATA_HEBREW: HeroCard[] = Array.from(
+  { length: 3 },
+  (_, index) => ({
+    id: `Hebrew-${index + 1}`,
+    title: `Title ${index + 1}`,
+    description: `Description for Hebrew ${index + 1}.`,
+    order: index + 1,
+    active: index % 2 === 0,
+    image: `image${index + 1}.jpg`,
+    action: `http://www.hebrew.com/${index + 1}`,
+  })
 );
